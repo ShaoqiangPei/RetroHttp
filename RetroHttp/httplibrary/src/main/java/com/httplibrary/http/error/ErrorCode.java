97324299c7775bd747114179292a8cc47c14f89e
LAPTOP-4NDLVHMD\pain <769936726@qq.com>
2019-12-20 11:44:23 +0800

    1. 添加一些特殊的错误提示

--- RetroHttp/httplibrary/src/main/java/com/httplibrary/http/error/ErrorCode.java
+++ RetroHttp/httplibrary/src/main/java/com/httplibrary/http/error/ErrorCode.java
@@ -14,13 +14,20 @@ public class ErrorCode {
 
     private static Map<Integer, String> mErrorMap=new HashMap<>();
 
-    public static final int NO_NET_WORK=-1;//网络未连接，请开启网络后使用
-    public static final int NETWORK_EXCEPTION_CODE=-2;//通讯数据解析异常:通讯onNext(Object obj)返回数据为空
-    public static final int SERVER_FAILED_CODE=-3;//通讯发生错误,会有自己的message,通讯进入onError()流程
+    /**通讯发生错误,会有自己的message,通讯进入onError()流程**/
+    public static final int SERVER_FAILED_CODE=-1;//通讯发生错误,会有自己的message,通讯进入onError()流程
+
+    /**此库内置code定义**/
+    public static final int NO_NET_WORK=-2;//网络未连接，请开启网络后使用
+    public static final int NETWORK_EXCEPTION_CODE=-3;//通讯数据解析异常:通讯onNext(Object obj)返回数据为空
+    public static final int HTTP_HPPS_CODE=-4;//通讯为http请求头不兼容,请更换为https请求头
+    public static final int CONNECT_TIME_OUT_CODE=-5;//通讯连接超时
+
 
     private static Map<Integer, String> getErrorMap(){
         mErrorMap.put(NO_NET_WORK,"网络未连接,请开启网络后使用!");
         mErrorMap.put(NETWORK_EXCEPTION_CODE,"通讯数据解析异常");
+        mErrorMap.put(HTTP_HPPS_CODE,"通讯为http请求头不兼容,请更换为https请求头");
         return mErrorMap;
     }
 
--- RetroHttp/httplibrary/src/main/java/com/httplibrary/http/error/HttpRepose.java
+++ RetroHttp/httplibrary/src/main/java/com/httplibrary/http/error/HttpRepose.java
@@ -1,6 +1,8 @@
 package com.httplibrary.http.error;
 
+import com.httplibrary.R;
 import com.httplibrary.http.rx.RxObserver;
+import com.httplibrary.util.StringUtil;
 
 /**
  * Title:统一处理提示语
@@ -11,9 +13,12 @@ import com.httplibrary.http.rx.RxObserver;
  */
 public class HttpRepose {
 
-    /**
-     * 发生错误时，错误提示的统一处理
-     **/
+    //当前通讯为http请求头不兼容,请更换为https请求头[对应自定义code=ErrorCode.HTTP_HPPS_CODE]
+    private static final String HTTP_HPPS_MESSAGE="CLEARTEXT communication to appxd.inm.cn not permitted by network security policy";
+    //通讯连接超时[对应自定义code=ErrorCode.CONNECT_TIME_OUT_CODE]
+    private static final String CONNECT_TIME_OUT_MESSAGE="connect timed out";
+
+    /**发生错误时，错误提示的统一处理**/
     public static ServerException handle(ServerException exception) {
         int code = exception.getCode();
         String message = exception.getMessage();
@@ -23,11 +28,44 @@ public class HttpRepose {
             case ErrorCode.NETWORK_EXCEPTION_CODE://通讯数据解析异常
                 message = ErrorCode.getErrorMessageByCode(code);
                 break;
+            case ErrorCode.SERVER_FAILED_CODE://通讯发生错误,会有自己的message,通讯进入onError()流程
+                //针对此类错误中一些特殊情况的处理
+                ServerException specialException=getSpecialException(code,message);
+                if(specialException!=null){
+                    code=specialException.getCode();
+                    message=specialException.getMessage();
+                }
+                break;
             default:
                 break;
         }
+        exception.setCode(code);
         exception.setMessage(message);
         return exception;
+    }
+
+    /**针对ErrorCode.SERVER_FAILED_CODE这类错误中的一些特殊错误的处理**/
+    private static ServerException getSpecialException(int code, String message) {
+        String lastMessage = "(原错误为:" + message + ")";
+        if (StringUtil.isNotEmpty(message)) {
+            switch (message) {
+                case HTTP_HPPS_MESSAGE://当前通讯为http请求头不兼容,请更换为https请求头
+                    code = ErrorCode.HTTP_HPPS_CODE;
+                    message = ErrorCode.getErrorMessageByCode(code) + lastMessage;
+                    break;
+                case CONNECT_TIME_OUT_MESSAGE://通讯连接超时
+                    code = ErrorCode.CONNECT_TIME_OUT_CODE;
+                    message = ErrorCode.getErrorMessageByCode(code) + lastMessage;
+                    break;
+                default:
+                    break;
+            }
+            ServerException exception = new ServerException();
+            exception.setCode(code);
+            exception.setMessage(message);
+            return exception;
+        }
+        return null;
     }
 
 }
